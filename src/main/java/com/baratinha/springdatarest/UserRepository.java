83dd03e49a06889213e629fca7036911d2bf5bec
lucas <1qaz2wsx>
2019-05-16 15:05:43 -0300

    first commit

---
+++ src/main/java/com/baratinha/springdatarest/UserRepository.java
@@ -0,0 +1,12 @@
+package com.baratinha.springdatarest;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<WebsiteUser, Long> {
+    List<WebsiteUser> findByName(@Param("name") String name);
+}
